1fc89b1dbc34421a82b9a67f56e6c4ee15623227
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-02 17:25:03 +0530

    Added assignment numbers

--- src/Assign1/Main.java
+++ src/Assign1/Main.java
@@ -1,3 +1,10 @@
+// Design a class „Complex‟ with data members for real and imaginary part. Provide default and
+// Parameterized constructors. Write a program to perform arithmetic operations of two complex numbers.
+
+//Name : Tajjuddin Malnas
+//Roll : SYITB070
+//No   : Assignment No 2
+
 package Assign1;
 import java.util.*;
 public class Main {
